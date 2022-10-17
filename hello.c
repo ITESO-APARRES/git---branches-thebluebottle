558f67a254903f9ed65afda886b99854decaf6e0
Brandon mendoza <brandon_amm@hotmail.com>
2022-10-16 19:19:21 -0500

    New file hello.c

---
+++ hello.c
@@ -0,0 +1,14 @@
+//
+//  hello.c
+//  branches demo
+//
+//  Created by Brandon Mendoza Medina on 16/10/22.
+//
+
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello world!\n");
+    return 0;
+}
+
