b71a749ffa78948dd64e41c94dc366d7baa7b844
Brandon mendoza <brandon_amm@hotmail.com>
2022-10-16 19:43:38 -0500

    math.c and math.h

---
+++ math.h
@@ -0,0 +1,15 @@
+//
+//  math.h
+//  branches demo
+//
+//  Created by Brandon Mendoza Medina on 16/10/22.
+//
+
+#ifndef math_h
+#define math_h
+
+#define MAX_INT (2^sizeof(int))/2
+
+int dobleInt(int a);
+
+#endif /* math_h */
